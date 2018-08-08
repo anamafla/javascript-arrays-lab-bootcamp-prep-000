2a98222ba5fc0c54bf6de4de675048b65eff31de
Ana Mafla <anamafla@hotmail.com>
2018-08-08 15:09:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,3 +30,8 @@ function removeLastKitten(){
   let newArray = kittens.slice(0, kittens.length-1)
   return newArray;
 }
+
+function removeFirstKitten(){
+  let newArray = kittens.slice(1)
+  return newArray;
+}
