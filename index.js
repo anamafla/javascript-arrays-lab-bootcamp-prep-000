841d4ed84071f1cd78dedadc08a30bc9e8116e77
Ana Mafla <anamafla@hotmail.com>
2018-08-08 15:03:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,3 +15,8 @@ function destructivelyRemoveLastKitten(){
 function destructivelyRemoveFirstKitten(){
   kittens.shift()
 }
+
+function appendKitten(name){
+  let newArray = [...kittens, name]
+  return newArray;
+}
